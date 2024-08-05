13234551d9c45dd5f82105c680fd3e60585e173e
James Riley III <james.h.t.riley@gmail.com>
2024-08-04 20:25:23 -0400

    first commit

--- firebase.js
+++ firebase.js
@@ -1,12 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBbSZSCsGj7f63qmN4mxeOZIaImsdacM_A",
     authDomain: "inventory-afcb5.firebaseapp.com",
@@ -19,5 +15,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const firestore = getAnalytics(app);
+const firestore = getFirestore(app);
 export { firestore };
